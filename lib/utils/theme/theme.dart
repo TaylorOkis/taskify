cc0a1aba6b0224cd861523b106327bb8c94580b7
TaylorOkis <taylorokis@yahoo.com>
2024-09-25 03:44:39 +0100

    Fix error in theme.dart file

--- lib/utils/theme/theme.dart
+++ lib/utils/theme/theme.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:taskify/utils/theme/custom/floating_action_button.dart';
 
 import 'custom/checbox.dart';
 import 'custom/color_scheme.dart';
